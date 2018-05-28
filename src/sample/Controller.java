e81ecf01e4b400d1d8820e9d7ce42c588a54e2e9
DevSergei <bakaikin@mail.ru>
2018-05-28 11:29:28 +0300

    private filed issue

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,22 +6,17 @@ import javafx.scene.control.TextField;
 
 import java.util.Map;
 
-
 public class Controller {
-
     @FXML
     public TextArea textArea;
-
     @FXML
     public TextField textFieldForNumber;
-
     @FXML
     public TextField textFieldForName;
-
     TelDirectory telDirectory = new TelDirectory();
 
     @FXML
-    private void addToDirect(){
+    private void addToDirect() {
         String strNumber = textFieldForNumber.getText();
         String strName = textFieldForName.getText();
         telDirectory.add(strNumber, strName);
@@ -31,24 +26,19 @@ public class Controller {
     }
 
     @FXML
-    private void getNumber(){
+    private void getNumber() {
         String strName = textFieldForName.getText();
         textArea.clear();
-        for(String str : telDirectory.get(strName)){
+        for (String str : telDirectory.get(strName)) {
             textArea.appendText(str + "\n");
         }
     }
 
     @FXML
-    private void showAllNum(){
+    private void showAllNum() {
         textArea.clear();
-        for(Map.Entry<String,String> entry : telDirectory.telDirMap.entrySet()){
+        for (Map.Entry<String, String> entry : telDirectory.telDirMap.entrySet()) {// accessing directly a field that should be private
             textArea.appendText(entry.getValue() + " " + entry.getKey() + "\n");
         }
     }
-
-
-
-
-
 }
